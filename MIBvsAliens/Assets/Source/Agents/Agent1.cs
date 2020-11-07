d613764d2289a66e0785fb8cbb365e8c7f17158d
DdarkSideE <ddarksidee@gmail.com>
2020-11-07 17:13:49 +0100

    added agent factories

---
+++ MIBvsAliens/Assets/Source/Agents/Agent1.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent1 : BaseAgent
+{
+    void Start()
+    {
+        type = AgentType.Type1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ MIBvsAliens/Assets/Source/Agents/AgentFactory2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class AgentFactory2 : IAgentFactory
+{
+    public BaseAgent Create()
+    {
+        throw new NotImplementedException();
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
