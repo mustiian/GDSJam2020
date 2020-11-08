528e3d7c0539db03067db0b0fb8790d69ec8b9ac
Ian Mustiats <mustiian@fit.cvut.cz>
2020-11-08 14:19:11 +0100

    Add more agents

--- MIBvsAliens/Assets/Scripts/Agents/AgentChooser.cs
+++ MIBvsAliens/Assets/Scripts/Agents/AgentChooser.cs
@@ -7,12 +7,16 @@ public class AgentChooser : MonoBehaviour
 {
     public Button buttonAgent1;
     public Button buttonAgent2;
+    public Button buttonAgent3;
+    public Button buttonAgent4;
 
     public IAgentFactory agentFactory;
     void Start()
     {
         buttonAgent1.onClick.AddListener(() => ChangeCurrentAgentFactory(FindObjectOfType<AgentFactory1>()));
         buttonAgent2.onClick.AddListener(() => ChangeCurrentAgentFactory(FindObjectOfType<AgentFactory2>()));
+        buttonAgent3.onClick.AddListener(() => ChangeCurrentAgentFactory(FindObjectOfType<AgentFactory3>()));
+        buttonAgent4.onClick.AddListener(() => ChangeCurrentAgentFactory(FindObjectOfType<AgentFactory4>()));
     }
     
     void ChangeCurrentAgentFactory(IAgentFactory newAgentFactory)
--- MIBvsAliens/Assets/Scripts/Agents/Factory/AgentFactory1.cs
+++ MIBvsAliens/Assets/Scripts/Agents/Factory/AgentFactory1.cs
@@ -4,15 +4,15 @@ public class AgentFactory1 : MonoBehaviour, IAgentFactory
 {
     public GameObject prefab;
 
-    public BaseAgent Create(Vector3 startPosition, Vector3 endPosition)
+    public BaseAgent Create(Vector3 position, Vector3 endPosition)
     {
         Agent1 agent = prefab.GetComponent<Agent1>();
 
         if (GameManager.instance.pointsManager.HasRequiredPoints(agent.cost))
         {
-            var gameObject = Instantiate(prefab, startPosition, Quaternion.identity);
+            var gameObject = Instantiate(prefab, position, Quaternion.identity);
             var movement = gameObject.GetComponent<Movement>();
-            movement.SetStartPosition(startPosition);
+            movement.SetStartPosition(position);
             movement.SetDestination(endPosition);
             return agent;
         }
