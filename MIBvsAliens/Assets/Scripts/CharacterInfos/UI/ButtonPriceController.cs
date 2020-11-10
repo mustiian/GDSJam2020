37228bbe344c2f8f29d34b958c3bb2b3586b8656
Andrei Gavrilov <ddarksidee@gmail.com>
2020-11-10 20:42:40 +0100

    buttons selected

--- MIBvsAliens/Assets/Scripts/CharacterInfos/UI/ButtonPriceController.cs
+++ MIBvsAliens/Assets/Scripts/CharacterInfos/UI/ButtonPriceController.cs
@@ -1,12 +1,17 @@
 ﻿using System;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
+using Button = UnityEngine.UI.Button;
 
 public class ButtonPriceController : MonoBehaviour
 {
     public AgentType agentType;
     private int _price;
     private Button _button;
+    private ButtonPriceController[] _allButtonControllers;
+    private ColorBlock _defaultColors;
+
     private void Start()
     {
         var info = GameManager.instance.agentsInfoGetter.GetFor(agentType);
@@ -15,12 +20,28 @@ public class ButtonPriceController : MonoBehaviour
 
         _price = info.cost;
         _button = GetComponentInParent<Button>();
+        _button.onClick.AddListener(Call);
+        _defaultColors = _button.colors;
+        
+        var panelTag = FindObjectOfType<PanelTag>();
+        var buttons = panelTag.gameObject.GetComponentsInChildren<Button>();
+        _allButtonControllers = buttons.Where(b => b.name.StartsWith("Agent"))
+            .Select(b => b.GetComponentInChildren<ButtonPriceController>()).ToArray();
+    }
+
+    private void Call()
+    {
+        foreach (var controller in _allButtonControllers)
+        {
+            controller.Deselect();
+        }
+        Select();
     }
 
     private void FixedUpdate()
     {
         var available = GameManager.instance.pointsManager.HasRequiredPoints(_price);
-        if (_button.enabled != available)
+        if (_button.interactable != available)
         {
             if (available)
                 EnableButton();
@@ -31,11 +52,24 @@ public class ButtonPriceController : MonoBehaviour
 
     private void DisableButton()
     {
-        _button.enabled = false;
+        _button.interactable = false;
     }
     
     private void EnableButton()
     {
-        _button.enabled = true;
+        _button.interactable = true;
+    }
+
+    private void Select()
+    {
+        var colors = _button.colors;
+        colors.normalColor = new Color(_defaultColors.pressedColor.r, _defaultColors.pressedColor.g,
+            _defaultColors.pressedColor.b, _defaultColors.pressedColor.a);
+        _button.colors = colors;
+    }
+
+    private void Deselect()
+    {
+        _button.colors = _defaultColors;
     }
 }
